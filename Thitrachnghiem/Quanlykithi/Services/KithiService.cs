c485fb5823bae0479f218cdbfbf9c50833038d36
lanhuong1598 <lanhuongitk55lvt@gmail.com>
2021-12-08 08:03:15 +0700

    update

--- Thitrachnghiem/Quanlykithi/Services/KithiService.cs
+++ Thitrachnghiem/Quanlykithi/Services/KithiService.cs
@@ -328,7 +328,7 @@ namespace Thitrachnghiem.Quanlykithi.Services
             F_Phienthi f_Phienthi = new F_Phienthi();
             Phienthi phienthi = f_Phienthi.GetPhienthiDangMoByKithi(kithi.Id);
             phienthi.Kithiid = kithi.Id;
-            phienthi.Thoigianbatdau = DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss");
+            phienthi.Thoigianketthuc = DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss");
 
             kithi.Dangthi = false;
             F_Kithi.Update(kithi);
